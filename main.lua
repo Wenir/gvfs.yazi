7f01148affa0fe985d785d9a7167b861770fd09a
Huy Hoang <boydaihungst@gmail.com>
2026-07-13 20:34:14 +0700

    fix: adapt to new yazi API: url.spec

--- main.lua
+++ main.lua
@@ -1846,7 +1846,7 @@ local save_tab_hovered = ya.sync(function()
 	local hovered_item_per_tab = {}
 	for _, tab in ipairs(cx.tabs) do
 		local is_virtual = (Url(tab.current.cwd).spec and Url(tab.current.cwd).spec.is_virtual)
-			or (Url(tab.current.cwd).scheme and Url(tab.current.cwd).scheme.is_virtual)
+			or (not Url(tab.current.cwd).spec and Url(tab.current.cwd).scheme.is_virtual)
 		table.insert(hovered_item_per_tab, {
 			id = (type(tab.id) == "number" or type(tab.id) == "string") and tab.id or tab.id.value,
 			cwd = tostring((is_virtual and tab.current.cwd or tab.current.cwd.path) or tab.current.cwd),
@@ -1865,7 +1865,7 @@ local redirect_unmounted_tab_to_home = ya.sync(function(_, unmounted_url, notify
 	end
 	for _, tab in ipairs(cx.tabs) do
 		local is_virtual = (Url(tab.current.cwd).spec and Url(tab.current.cwd).spec.is_virtual)
-			or (Url(tab.current.cwd).scheme and Url(tab.current.cwd).scheme.is_virtual)
+			or (not Url(tab.current.cwd).spec and Url(tab.current.cwd).scheme.is_virtual)
 		if ((is_virtual and tab.current.cwd or tab.current.cwd.path) or tab.current.cwd):starts_with(unmounted_url) then
 			ya.emit("cd", {
 				HOME,
@@ -2247,7 +2247,7 @@ end
 local function toggle_automount_when_cd_action(enabled)
 	local hovered_path = get_hovered_path()
 	local is_virtual = (Url(hovered_path).spec and Url(hovered_path).spec.is_virtual)
-		or (Url(hovered_path).scheme and Url(hovered_path).scheme.is_virtual)
+		or (not Url(hovered_path).spec and Url(hovered_path).scheme.is_virtual)
 	if is_virtual then
 		return
 	end
